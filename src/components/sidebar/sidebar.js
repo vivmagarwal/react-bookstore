a80a5dc20377a3c415241d9834fae5e6d3e311e6
Vivek Agarwal <vivmagarwal@gmail.com>
2022-02-07 15:34:34 +0530

    Initialize project-phase-7

---
+++ src/components/sidebar/sidebar.js
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import SidebarVisibilityContext from "../../store/sidebarVisibilityContext";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../store/userContext";
+
+function Sidebar() {
+  const [sidebarVisibility,, toggleSidebarVisibility] = useContext(SidebarVisibilityContext);
+  const { user } = useContext(UserContext);
+
+  return (
+    <>
+      <div id="sidebar" className={`sidebar ${sidebarVisibility ? 'active' : ''}`}>
+        <button onClick={toggleSidebarVisibility} className="btn--clear hamburger hamburger--light hamburger--cross sidebar__toggle-sidebar">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <nav>
+          <ul>
+            <li> <Link to="/" >Home</Link> </li>
+            <li> <Link to="/list">List</Link> </li>
+            <li> <Link to="/book">Book</Link> </li>
+            {user && 
+              <li> <Link to="/create">Create A Book</Link> </li>
+            }
+          </ul>
+        </nav>
+
+      </div>
+    </>
+  )
+}
+
+export default Sidebar;
---
+++ src/pages/create.js
@@ -0,0 +1,67 @@
+import { useContext, useState } from 'react';
+import { UserContext } from '../store/userContext';
+
+function Create() {
+  const [bookTitle, setBookTitle] = useState('');
+  const [favSubject, setFavSubject] = useState('Javascript');
+  const [err, setError] = useState('');
+  const { user, setUser } = useContext(UserContext);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!bookTitle) {
+      setError('Book title is required.')
+      return;
+    }
+
+    if (!favSubject) {
+      setError('FavSubject is required.')
+      return;
+    }
+
+    try {
+      const respose = await fetch('https://iifsd.herokuapp.com/studentss', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: bookTitle,
+          favSubject: favSubject,
+        }),
+      });
+
+      if (!respose.ok) {
+        throw new Error("Server responds with error!")
+      }
+
+      const data = await respose.json();
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+  }
+
+  return (
+    <div className="page__create">
+      
+      ERR:{err && <p className="msg msg--error">{err.toString()}</p>}
+      <br />
+      USER: <p>{JSON.stringify(user)}</p>
+      
+
+      <h1>Create a Book</h1>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="book title" value={bookTitle} onChange={(e) => { setError(''); setBookTitle(e.target.value)}} />
+        <input placeholder="fav subject" value={favSubject} onChange={(e) => { setError(''); setFavSubject(e.target.value) }} />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Create;
