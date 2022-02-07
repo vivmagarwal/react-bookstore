1e75a4020bc80fea6763c6a3433996e5c5e3ae3e
Vivek Agarwal <vivmagarwal@gmail.com>
2022-02-07 15:34:33 +0530

    Initialize project-phase-5

---
+++ src/components/sidebar/sidebar.js
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+import SidebarVisibilityContext from "../../store/sidebarVisibilityContext";
+import { Link } from "react-router-dom";
+
+function Sidebar() {
+  const [sidebarVisibility,, toggleSidebarVisibility] = useContext(SidebarVisibilityContext);
+
+  return (
+    <>
+      <div id="sidebar" className={`sidebar ${sidebarVisibility ? 'active' : ''}`}>
+        <button onClick={toggleSidebarVisibility} className="btn--clear hamburger hamburger--light hamburger--cross sidebar__toggle-sidebar">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" >Home</Link>
+            </li>
+            <li>
+              <Link to="/list">List</Link>
+            </li>
+            <li>
+              <Link to="/book">Book</Link>
+            </li>
+            <li>
+              <Link to="/create">Create</Link>
+            </li>
+          </ul>
+        </nav>
+
+      </div>
+    </>
+  )
+}
+
+export default Sidebar;
